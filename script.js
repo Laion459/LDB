54e54add8dadbf22b919baf2f257254bfc4fbd6d
Leonardo Borges <54716998+Laion459@users.noreply.github.com>
2023-12-07 23:13:17 -0300

    Add files via upload
    v1

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript para navegação suave entre as seções
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
