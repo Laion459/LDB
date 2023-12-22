c8f062291deade8e1ff0430e7eb4fb8004007249
laion459 <laion459@gmail.comgit config --global user.email laion459@gmail.com>
2023-12-21 23:48:18 -0300

    upgrade CRUD

--- projetos/CRUD React Node Mysql/api/index.js
+++ projetos/CRUD React Node Mysql/api/index.js
@@ -1,9 +1,15 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import userRoutes from "./routes/users.js";
+
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
-app.listen(8800);
+
+app.use("/", userRoutes);
+
+app.listen(4000);
+
