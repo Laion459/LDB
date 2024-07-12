c85e8c5fefd47fa5208a4aaac9658d2c71497b42
Laion459 <laion459@gmail.com>
2024-07-12 19:52:40 -0300

    upgrade css home page

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ document.addEventListener('DOMContentLoaded', function () {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+
+// Função para atualizar o ano automaticamente no rodapé
+function updateCopyrightYear() {
+    const copyrightYearElement = document.querySelector('footer .container p');
+    const currentYear = new Date().getFullYear();
+    copyrightYearElement.textContent = `© ${currentYear} Leonardo Borges. Todos os direitos reservados.`;
+}
+
+// Chama a função para atualizar o ano ao carregar a página
+window.onload = updateCopyrightYear;
