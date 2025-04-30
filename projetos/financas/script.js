3d15a4875a02318703bf7df3ec51d0353d19bb25
Laion459 <laion459@gmail.com>
2025-04-30 19:10:31 -0300

    new ideas

---
+++ projetos/financas/script.js
@@ -0,0 +1,39 @@
+// Mover o projeto quando o mouse passar sobre ele
+function moveProject(project) {
+    project.style.transform = 'translateY(-10px)';
+}
+
+// Resetar a posição do projeto quando o mouse sair dele
+function resetProject(project) {
+    project.style.transform = 'translateY(0)';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var backButton = document.querySelector('.back-to-top');
+
+    // Adicione um ouvinte de rolagem para verificar a posição da página
+    window.addEventListener('scroll', function () {
+        // Se o usuário rolar mais de 200 pixels, mostra o botão, caso contrário, esconde
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            backButton.style.display = 'block';
+        } else {
+            backButton.style.display = 'none';
+        }
+    });
+});
+
+// Função para rolar suavemente ao topo
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+// Função para atualizar o ano automaticamente no rodapé
+function updateCopyrightYear() {
+    const copyrightYearElement = document.querySelector('footer .container p');
+    const currentYear = new Date().getFullYear();
+    copyrightYearElement.textContent = `© ${currentYear} Leonardo Borges. Todos os direitos reservados.`;
+}
+
+// Chama a função para atualizar o ano ao carregar a página
+window.onload = updateCopyrightYear;
