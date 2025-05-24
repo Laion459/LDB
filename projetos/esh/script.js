7fc580e7e37b52b8564087228efda9e682a31a44
Laion459 <laion459@gmail.com>
2025-05-24 00:59:19 -0300

    Add .gitignore file

---
+++ projetos/esh/script.js
@@ -0,0 +1,12 @@
+// Adicione aqui qualquer JavaScript para interatividade
+
+// Exemplo: rolagem suave para links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
---
+++ projetos/esh/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Ajuste os caminhos conforme necessário
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
